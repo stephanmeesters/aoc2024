c5b0c4dc85d211c565a6140d57e57771937d6381
Stephan Meesters <stephan.meesters@gmail.com>
2024-12-02 17:52:43 +0100

    day2-amendment

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,12 +1,11 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let mut num_safe = 0;
     for line in input.lines() {
-        let levels: Vec<i32> = line
-            .split_whitespace()
-            .map(|k| k.parse().unwrap())
-            .collect();
+        let levels = line.split_whitespace().map(|k| k.parse().unwrap());
         if check_ok(levels) {
             num_safe += 1;
         }
@@ -23,7 +22,7 @@ pub fn part_two(input: &str) -> Option<u32> {
             .collect();
 
         for i in 0..levels.len() {
-            let subset:Vec<i32> = levels.clone().into_iter().enumerate().filter(|&(j,_)| i != j).map(|(_,v)| v).collect();
+            let subset = levels.iter().enumerate().filter(|&(j,_)| i != j).map(|(_,v)| *v);
             if check_ok(subset) {
                 num_safe += 1;
                 break;
@@ -33,9 +32,9 @@ pub fn part_two(input: &str) -> Option<u32> {
     Some(num_safe)
 }
 
-fn check_ok(input: Vec<i32>) -> bool {
-    let diffs: Vec<i32> = input.windows(2).map(|w| w[0] - w[1]).collect();
-    if !diffs.iter().all(|d| d.abs() >= 1 && d.abs() <= 3) {
+fn check_ok<I: Iterator<Item = i32>>(input: I) -> bool {
+    let diffs: Vec<i32> = input.tuple_windows().map(|(a,b)| a - b).collect();
+    if diffs.iter().any(|d| d.abs() < 1 && d.abs() > 3) {
         return false;
     }
     let signs_positive = diffs.iter().map(|d| d.signum()).all(|s| s == 1);
