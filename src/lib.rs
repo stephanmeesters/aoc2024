6bebd9eb4c944f3995d3c7c755897b1b87ec254e
Stephan Meesters <stephan.meesters@gmail.com>
2024-12-08 14:38:21 +0100

    day8

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+//
+#[macro_export]
+macro_rules! printd {
+    ($val:expr) => {
+        println!("{:?}", $val);
+    };
+}
+
